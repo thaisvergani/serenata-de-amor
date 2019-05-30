7b4fbae1f1e43099d430bd12698b7e76a799954b
Thais Vergani <thais.vergani@gmail.com>
2019-05-30 17:40:31 -0300

    filtrar gráfico por parlamentar

--- jarbas/dataviz/urls.py
+++ jarbas/dataviz/urls.py
@@ -7,8 +7,7 @@ app_name = 'dataviz'
 
 urlpatterns = [
     url(r'^$', views.dataviz_dashboard),
-    url(r'^api/tweet_chart/', views.tweet_chart, name='tweet_chart'),
-    url(r'^meals_chart/', views.meals_chart, name='meals_chart'),
-    url(r'^api/meals_chart/', views.meals_chart_data, name='meals_chart_data'),
+    url(r'^api/tweet_chart', views.tweet_chart, name='tweet_chart'),
+    url(r'^api/congressperson_info', views.get_congress_person_data, name='congressperson_info'),
 
 ]
--- jarbas/dataviz/views.py
+++ jarbas/dataviz/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 
-from jarbas.chamber_of_deputies.models import Reimbursement, Tweet
+from jarbas.chamber_of_deputies.models import Reimbursement, Tweet, SocialMedia
 import datetime
 import os
 import pandas as pd
 import json
+import requests
 from django.http import JsonResponse, HttpResponse
 
 import twitter
@@ -39,24 +40,12 @@ def dataviz_dashboard(request):
 
 
 def get_twitter_data():
-    api_url = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
-    consumer_key = '7VaADONcHBiai9TS8YJ05mcTe'
-    consumer_secret = 'C58ej1UQF5tg0hd7y54m31CuOUMMFuQoIiGfxc6VnGvNgeJYj6'
-    access_token = '1030805473426063361-jlNN17oS2osQshKmp94GsrPeNJpwGw'
-    access_token_secret = 'Lm7Nmw49oAYPd4ZqActTOpapmC6MTiQP4EElT583sMYMB'
-
-    kwargs = dict(
-        screen_name='RosieDaSerenata',
-        count=200,  # this is the maximum suported by Twitter API
-        include_rts=False,
-        exclude_replies=True
-    )
-
     api = twitter.Api(consumer_key='7VaADONcHBiai9TS8YJ05mcTe',
                       consumer_secret='C58ej1UQF5tg0hd7y54m31CuOUMMFuQoIiGfxc6VnGvNgeJYj6',
                       access_token_key='1030805473426063361-jlNN17oS2osQshKmp94GsrPeNJpwGw',
                       access_token_secret='Lm7Nmw49oAYPd4ZqActTOpapmC6MTiQP4EElT583sMYMB',
                       sleep_on_rate_limit=True)
+
     tweets = api.GetUserTimeline(
         screen_name='RosieDaSerenata',
         count=200,  # this is the maximum suported by Twitter API
@@ -69,97 +58,10 @@ def get_twitter_data():
     return df_tweets
 
 
-def meals_chart(request):
-    return render(request=request,
-                  template_name="meals_chart.html",
-                  )
-
-
-def meals_chart_data(request):
-    tweets = Tweet.objects.all().values()
-    tweets_df = pd.DataFrame(list(tweets))
-    tweet_reimbursements_list = tweets_df['reimbursement_id'].tolist()
-    subquota_number = 13
-
-    tweet_reimbursements = Reimbursement.objects.filter(id__in=tweet_reimbursements_list,
-                                                        subquota_number=subquota_number)
-
-    df = pd.DataFrame(list(tweet_reimbursements.values('document_id',
-                                                       'year',
-                                                       'total_value',
-                                                       'total_net_value',
-                                                       'congressperson_id',
-                                                       'suspicions',
-                                                       'issue_date',
-                                                       'month',
-                                                       'document_value',
-                                                       'subquota_number',
-                                                       )))
-
-    print(df)
-    df = df.fillna(0)
-    df['issue_date'] = pd.to_datetime(df.issue_date)
-    df['issue_date'] = df['issue_date'].dt.strftime('%m/%d/%Y')
-    df = df.sort_values(['issue_date'])
-
-    tweet_reimbursements = df.to_dict(orient='records')
-
-    # return JsonResponse([
-    #     {"year": "2005", "value": 771900},
-    #     {"year": "2006", "value": 771500},
-    #     {"year": "2007", "value": 770500},
-    #     {"year": "2008", "value": 770400},
-    #     {"year": "2009", "value": 771000},
-    #     {"year": "2010", "value": 772400},
-    #     {"year": "2011", "value": 774100},
-    #     {"year": "2012", "value": 776700},
-    #     {"year": "2013", "value": 777100},
-    #     {"year": "2014", "value": 779200},
-    #     {"year": "2015", "value": 782300}
-    # ],
-    #     safe=False)
-
-    return JsonResponse(tweet_reimbursements,
-                        safe=False)
-    #
-    # context={'max_reimbursement': float(df['total_net_value'].max()),
-    #                        'min_date': tweet_reimbursements[0]['year'],
-    #                        'max_date': tweet_reimbursements[-1]['year'],
-    #                        'tweet_reimbursements':  df.to_json(orient='records')})
-
-    min_year = df['year'].min()
-
-    congresspeople = set(df['congressperson_id'].tolist())
-
-    reimbursements = Reimbursement.objects.filter(
-        subquota_number=subquota_number,
-        congressperson_id__in=congresspeople
-    )
-
-    reimbursements_df = pd.DataFrame(list(reimbursements.values('document_id',
-                                                                'year',
-                                                                'total_value',
-                                                                'total_net_value',
-                                                                'congressperson_id')))
-
-    return render(request=request,
-                  template_name="dashboard.html",
-                  )
-
-    data = pd.DataFrame()
-    current_path = os.getcwd()
-    # years = [year for year in range(2008, 2020)]
-    # for year in years:
-    #     print(year)
-    #     temp = pd.read_csv(f'{current_path}/serenata-data/reimbursements-{year}.csv', low_memory=False)
-    #     data = pd.concat([data, temp])
-    # del (temp)
-
-
 def tweet_chart(request):
     tweets = Tweet.objects.all().values()
     all_tweets_df = pd.DataFrame(list(tweets))
-    congressperson_id = 171620
+    congressperson_id = request.GET.get('congressperson_id')
     tweet_reimbursements_list = all_tweets_df['reimbursement_id'].tolist()
     subquota_number = 13
 
@@ -178,6 +80,8 @@ def tweet_chart(request):
         'year',
         'month',
         'issue_date',
+        'total_net_value',
+        'suspicions'
     )))
 
     df_tweets_reimbursements = pd.merge(df_tweets_reimbursements,
@@ -217,6 +121,7 @@ def tweet_chart(request):
     df_all_reimbursements = df_all_reimbursements[['reimbursement',
                                                    'value',
                                                    'issue_date',
+                                                   'suspicions',
                                                    'congressperson_name',
                                                    'congressperson_id']]
 
@@ -225,10 +130,10 @@ def tweet_chart(request):
     len_tweets = len(tweetdates)
     means = pd.DataFrame(columns=["initial_date", "final_date", "mean"])
 
-    df_tweets_reimbursements=df_tweets_reimbursements.sort_values(by='issue_date')
+    df_tweets_reimbursements = df_tweets_reimbursements.sort_values(by='issue_date')
     row_iterator = df_tweets_reimbursements.iterrows()
 
-    for  index, (i, data) in enumerate(row_iterator):
+    for index, (i, data) in enumerate(row_iterator):
 
         start_date = data.issue_date
         print(index)
@@ -243,17 +148,19 @@ def tweet_chart(request):
             ]
         mean_per_day = period['value'].sum() / delta.days
         mean = period['value'].mean()
+        # from IPython import embed; embed()
         means = means.append(pd.DataFrame(
-            [[congressperson_id, start_date, final_date, mean_per_day, mean, int(data.status)]],
-            columns=["congressperson_id", "initial_date", "final_date", "mean_per_day", "mean", "tweet_id"],
-        ), sort=True )
+            [[congressperson_id, start_date, final_date, mean_per_day, mean, int(data.status),
+              float(data.total_net_value), list(data.suspicions.keys())]],
+            columns=["congressperson_id", "initial_date", "final_date", "mean_per_day", "mean", "tweet_id",
+                     "total_net_value", "suspicions"],
+        ), sort=True)
 
     means = pd.merge(means,
                      df_tweet_data,
                      left_on='tweet_id',
                      right_on='id',
                      how='left')
-    print(means[['initial_date', 'final_date']])
 
     means['initial_date'] = means['initial_date'].apply(lambda x: x.strftime('%m/%d/%Y'))
     means['final_date'] = means['final_date'].apply(lambda x: x.strftime('%m/%d/%Y'))
@@ -264,27 +171,41 @@ def tweet_chart(request):
         for congressperson_id in means['congressperson_id'].unique()
     ]
 
-    print(json.dumps(d))
-    print(d)
     return HttpResponse(
         json.dumps(d)
     )
 
 
-def beckup_all_reibursements(self):
-    df_reimb = 0
-    df_reimb = df_reimb.fillna(0)
+def get_congress_person_data(request):
+    congressperson_id = request.GET.get('congressperson_id')
+    try:
+        social_media = SocialMedia.objects.get(
+            congressperson_id=congressperson_id
+        )
+    except SocialMedia.DoesNotExist:
+        social_media = None
 
-    df_reimb = df_reimb.sort_values(by='issue_date', ascending=True)
-    df_reimb['issue_date'] = df_reimb['issue_date'].dt.strftime('%m/%d/%Y')
-    df_reimb['status'] = df_reimb['status'].astype(str)
+    # todo pode ser que nao tenha socialmedia
 
-    tweets = df_reimb[df_reimb['status'] != 0]
+    dados_abertos_api = "https://dadosabertos.camara.leg.br/api/v2/deputados/" + congressperson_id
+    # todo: define url to external api
 
-    tweet_reimbursements = df_reimb.to_dict(orient='index')
-    d = [
-        {'congressperson_id': str(congressperson_id),
-         'values': df_reimb[df_reimb['congressperson_id'] == congressperson_id].to_dict('records')}
-        for congressperson_id in df_reimb['congressperson_id'].unique()
-    ]
+    response = requests.get(dados_abertos_api, timeout=10)
+    response = response.json()
+    print(response)
+    c_info = {
+        'congressperson_name':  response.get('dados').get('ultimoStatus').get('nomeEleitoral'),
+        'twitter_profile': 'https://twitter.com' + social_media.twitter_profile if social_media else '',
+        'facebook_page': social_media.facebook_page if social_media else '',
+        'image_url': response.get('dados').get('ultimoStatus').get('urlFoto'),
+        'birthday': response.get('dados').get('dataNascimento'),
+        'status': response.get('dados').get('ultimoStatus').get('situacao'),
+        'scholarity': response.get('dados').get('escolaridade'),
+        'party': response.get('dados').get('ultimoStatus').get('siglaPartido'),
+        'uf': response.get('dados').get('ultimoStatus').get('siglaUf')
+    }
 
+    return render(request=request,
+                  template_name="congressperson.html",
+                  context=c_info
+                  )
